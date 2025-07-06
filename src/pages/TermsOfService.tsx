9afe73800a97bc33cb9786339981377ffe9c51f9
Misbah Sarfaraz <msbahsarfaraz@gmail.com>
2025-07-06 15:40:46 +0500

    Added meta tags to improve SEO

--- src/pages/TermsOfService.tsx
+++ src/pages/TermsOfService.tsx
@@ -20,6 +20,7 @@ const TermsOfService = () => {
         <meta name="robots" content="index, follow"/>
         <meta name="author" content="EnvHub Team"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="canonical" href="https://envhub.net/terms"/>
 
         {/* Open Graph */}
         <meta property="og:type" content="website"/>
